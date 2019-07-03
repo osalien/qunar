1b2dd7b19706881a926c561c1c008f5bab842853
yuanzhao <1578851175@qq.com>
2019-07-03 15:24:47 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="">
+      <div class="banner-info">
+        <div class="banner-title">大连圣亚海洋世界</div>
+        <div class="banner-number">
+          <span class='iconfont banner-icon'>&#xe692;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary
+      v-show="showGallary"
+      :imgs='imgs'
+      @close='handleGallaryClose'
+    ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: []
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.CommonGallary = true
+    },
+    handleGallaryClose () {
+      this.CommonGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
